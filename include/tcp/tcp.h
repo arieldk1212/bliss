98961da7d50233e661dac23559c0e0a499918ba1
Ariel <arieldk6@gmail.com>
2024-08-24 17:40:58 +0300

    stuff

--- include/tcp/tcp.h
+++ include/tcp/tcp.h
@@ -2,6 +2,18 @@
 #define TCP_H
 
 #include <sys/socket.h>
+#include <cstdint>
 
+// create basic client socket class, that will handle the socket and wait for responses from the user-agent server.
+class ClientSocket {
+private:
+  const char* m_socket_ip_address;
+  uint32_t m_socket_port;
+public:
+  ClientSocket(const char* ip, uint32_t port)
+  : m_socket_ip_address(ip), m_socket_port(port) {}
+
+
+};
 
 #endif
